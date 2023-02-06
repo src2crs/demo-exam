205d660d3240e122e49d6c5b3f99e770a3da57db
Reiner Hüchting <dev@reinerhuechting.de>
2023-02-06 17:16:32 +0100

    Add another example that enters an infinite loop.
    WIP: The interpreter cannot handle that case, yet.

---
+++ task1_submission7/task.go
@@ -0,0 +1,13 @@
+package task1
+
+import "fmt"
+
+// FilterGreaterValues expects a list l of integers and a number x,
+// and returns a list that contains only those elements from l that are greater that x.
+func FilterGreaterValues(l []int, x int) []int {
+	result := []int{}
+	for {
+		fmt.Println("I mustn't write infinite loops!")
+	}
+	return result
+}
